33f4415816b92331216635b078d6f6cc73ba52e0
rashsed60cyber <rashsed60@gmail.com>
2026-06-15 11:11:04 +0600

    Update and rename Circle. java to Circle.java

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -9,23 +9,24 @@ public class Circle extends Shape {
         this.radius = radius;
     }
 
+    @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
 
+    @Override
     public double getPerimeter() {
         return 2 * Math.PI * radius;
     }
 
+    @Override
     public String toString() {
         return "[ Circle ]\n" +
                super.toString() + "\n" +
                "Radius: " + radius + "\n" +
-               String.format("Area         : %.2f", getArea()) + "\n" +
+               String.format("Area         : %.2f", getArea())      + "\n" +
                String.format("Circumference: %.2f", getPerimeter());
     }
 
-    public double getRadius() {
-        return radius;
-    }
+    public double getRadius() { return radius; }
 }
