f6f8d8de5949607e3cb3aafecaac278ff42089e0
rashsed60cyber <rashsed60@gmail.com>
2026-06-15 10:43:40 +0600

    Implement Circle class with area and perimeter methods

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,1 +1,32 @@
 
+package shapes;
+
+public class Circle extends Shape {
+
+    private double radius;
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public String toString() {
+        return "[ Circle ]\n" +
+               super.toString() + "\n" +
+               "Radius: " + radius + "\n" +
+               String.format("Area         : %.2f", getArea()) + "\n" +
+               String.format("Circumference: %.2f", getPerimeter());
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+}
